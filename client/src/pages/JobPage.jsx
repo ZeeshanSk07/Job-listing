2d18b7f8e3ef37c8ada46f0e6b26e13aef97aed4
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2024-06-29 00:35:26 +0530

    built create job page

--- client/src/pages/JobPage.jsx
+++ client/src/pages/JobPage.jsx
@@ -18,12 +18,29 @@ const JobPage = ({ currentUser, setCurrentUser }) => {
 		}
 	}, [job]);
 
-	const timeElapsed = (createdAt) => {
+	const timeElapsedPretty = (createdAt) => {
 		const currentTime = new Date();
 		const createdAtTime = new Date(createdAt);
-		const difference = currentTime - createdAtTime;
-		return difference;
-		// return "1 day ago/20 minutes ago/3 weeks ago";
+		let differenceInMillis = currentTime - createdAtTime;
+		differenceInMillis = 21000000000;
+
+		const seconds = Math.floor(differenceInMillis / 1000);
+		const minutes = Math.floor(seconds / 60);
+		const hours = Math.floor(minutes / 60);
+		const days = Math.floor(hours / 24);
+		const weeks = Math.floor(days / 7);
+
+		if (weeks > 0) {
+			return weeks === 1 ? "1w ago" : `${weeks}w ago`;
+		} else if (days > 0) {
+			return days === 1 ? "1day ago" : `${days}days ago`;
+		} else if (hours > 0) {
+			return hours === 1 ? "1hr ago" : `${hours}hrs ago`;
+		} else if (minutes > 0) {
+			return minutes === 1 ? "1min ago" : `${minutes}min ago`;
+		} else {
+			return seconds === 1 ? "1sec ago" : `${seconds}sec ago`;
+		}
 	};
 
 	const fetchJob = async () => {
@@ -41,7 +58,7 @@ const JobPage = ({ currentUser, setCurrentUser }) => {
 			{job && (
 				<div>
 					{/* First Row */}
-					<p>{timeElapsed(job.createdAt)}</p>
+					<p>{timeElapsedPretty(job.createdAt)}</p>
 					<p>{job.jobType}</p>
 					<img src={job.logoUrl} alt="job logo" />
 					<p>{job.companyName}</p>
@@ -61,7 +78,6 @@ const JobPage = ({ currentUser, setCurrentUser }) => {
 					{job.skills.map((skill, index) => {
 						return <div key={index}>{skill}</div>;
 					})}
-					{/*Additional Information */}
 				</div>
 			)}
 		</div>
